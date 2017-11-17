704260c89220836d1b07994dd3d8cf83939ec0e5
PuszekHD <filder100@gmail.com>
2017-11-17 14:16:03 +0100

    added exercise files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = 23;
+var h = 17;
+var triangleArea = a*h/2;
+var finalScore = prompt('the final score of triangle field with base a=23 and base h=17 is', triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
